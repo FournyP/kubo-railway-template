685b3b46246fe2ad6a82ebca6b41e3ef166717b0
Pierre Fourny <pierre.fourny76@gmail.com>
2026-02-04 18:39:18 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# IPFS Kubo Railway Template
+
+This example deploys a server of [Kubo](https://github.com/ipfs/kubo).
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/kubo)
+
+## ✨ Features
+
+- Kubo
+
+## 💁‍♀️ How to use
+
+- Click the Railway button 👆
+- Fill in the variables
+- Deploy! 🚄
+
+## 📝 Notes
+
+- Source repo: https://github.com/FournyP/kubo-railway-template
+- Docs: https://docs.ipfs.tech/how-to
---
+++ kubo.config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+# Debugging: Print environment variables used in the script
+echo "KUBO_SWARM_TCP_ADDRESS_IPV4: $KUBO_SWARM_TCP_ADDRESS_IPV4"
+echo "KUBO_SWARM_TCP_ADDRESS_IPV6: $KUBO_SWARM_TCP_ADDRESS_IPV6"
+echo "KUBO_SWARM_UDP_ADDRESS_IPV4: $KUBO_SWARM_UDP_ADDRESS_IPV4"
+echo "KUBO_SWARM_UDP_ADDRESS_IPV6: $KUBO_SWARM_UDP_ADDRESS_IPV6"
+echo "KUBO_ANNOUNCE: $KUBO_ANNOUNCE"
+echo "KUBO_API_ADDRESS: $KUBO_API_ADDRESS"
+echo "DATA_PATH: $DATA_PATH"
+
+# Initialize IPFS if not already initialized
+if [ ! -f "$DATA_PATH/config" ]; then
+  echo "Initializing IPFS..."
+  ipfs init
+else
+  echo "IPFS already initialized."
+fi
+
+# Merge or update the config.json file
+ipfs config Addresses.Swarm "[\"${KUBO_SWARM_TCP_ADDRESS_IPV4}\", \"${KUBO_SWARM_TCP_ADDRESS_IPV6}\", \"${KUBO_SWARM_UDP_ADDRESS_IPV4}\", \"${KUBO_SWARM_UDP_ADDRESS_IPV6}\"]" --json
+ipfs config Addresses.Announce "[\"${KUBO_ANNOUNCE}\"]" --json
+ipfs config Addresses.API "[\"${KUBO_API_ADDRESS}\"]" --json
+ipfs config Routing.Type "dht"
+ipfs config Reprovider.Interval "30m"
+ipfs config Reprovider.Strategy "all"
