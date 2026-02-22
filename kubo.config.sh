210ab859d4ebc55b2ef5db3682d05a44772e3a09
Pierre Fourny <pierre.fourny76@gmail.com>
2026-02-22 13:07:24 +0100

    fix: update environment variable references from DATA_PATH to IPFS_PATH

--- kubo.config.sh
+++ kubo.config.sh
@@ -9,20 +9,20 @@ echo "KUBO_SWARM_UDP_ADDRESS_IPV4: $KUBO_SWARM_UDP_ADDRESS_IPV4"
 echo "KUBO_SWARM_UDP_ADDRESS_IPV6: $KUBO_SWARM_UDP_ADDRESS_IPV6"
 echo "KUBO_ANNOUNCE: $KUBO_ANNOUNCE"
 echo "KUBO_API_ADDRESS: $KUBO_API_ADDRESS"
-echo "DATA_PATH: $DATA_PATH"
+echo "IPFS_PATH: $IPFS_PATH"
 
 # Initialize IPFS if not already initialized
-if [ ! -f "$DATA_PATH/config" ]; then
+if [ ! -f "$IPFS_PATH/config" ]; then
   echo "Initializing IPFS..."
-  ipfs init
+  ipfs init --repo-dir="$IPFS_PATH"
 else
   echo "IPFS already initialized."
 fi
 
 # Merge or update the config.json file
-ipfs config Addresses.Swarm "[\"${KUBO_SWARM_TCP_ADDRESS_IPV4}\", \"${KUBO_SWARM_TCP_ADDRESS_IPV6}\", \"${KUBO_SWARM_UDP_ADDRESS_IPV4}\", \"${KUBO_SWARM_UDP_ADDRESS_IPV6}\"]" --json
-ipfs config Addresses.Announce "[\"${KUBO_ANNOUNCE}\"]" --json
-ipfs config Addresses.API "[\"${KUBO_API_ADDRESS}\"]" --json
-ipfs config Routing.Type "dht"
-ipfs config Provide.DHT.Interval "30m"
-ipfs config Provide.Strategy "all"
+ipfs --repo-dir="$IPFS_PATH" config Addresses.Swarm "[\"${KUBO_SWARM_TCP_ADDRESS_IPV4}\", \"${KUBO_SWARM_TCP_ADDRESS_IPV6}\", \"${KUBO_SWARM_UDP_ADDRESS_IPV4}\", \"${KUBO_SWARM_UDP_ADDRESS_IPV6}\"]" --json
+ipfs --repo-dir="$IPFS_PATH" config Addresses.Announce "[\"${KUBO_ANNOUNCE}\"]" --json
+ipfs --repo-dir="$IPFS_PATH" config Addresses.API "[\"${KUBO_API_ADDRESS}\"]" --json
+ipfs --repo-dir="$IPFS_PATH" config Routing.Type "dht"
+ipfs --repo-dir="$IPFS_PATH" config Provide.DHT.Interval "30m"
+ipfs --repo-dir="$IPFS_PATH" config Provide.Strategy "all"
